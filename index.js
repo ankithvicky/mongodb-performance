6a0d71cba2bb31a656a3895669e11e076244e16c
vigneshrajann <vigneshrajan@nextontop.com>
2020-01-02 10:48:07 +0530

    added update and delete options

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const url = 'mongodb://localhost:27017';
 const dbName = 'test';
 const _datacount = 10000;
 const _selectlimit = 10000;
-const _updateid = 100;
-const type = 'drop'; //insert , drop , selectone ,select , count , update
+const _updateid = 101;
+const type = 'delete'; //insert , drop , selectone ,select , count , update , updateone , deleteone , delete 
 // delete one, multiple
 // update multiple
 MongoClient.connect(url, function (err, client) {
@@ -15,11 +15,6 @@ MongoClient.connect(url, function (err, client) {
   switch (type) {
     case 'insert':
       insertDocuments(db, function () {
-        client.close();
-      });
-      break;
-    case 'drop':
-      dropCollections(db, function () {
         client.close();
       });
       break;
@@ -33,13 +28,33 @@ MongoClient.connect(url, function (err, client) {
         client.close();
       });
       break;
-    case 'count':
-      countCollections(db, function () {
+    case 'drop':
+      dropCollections(db, function () {
+        client.close();
+      });
+      break;
+    case 'updateone':
+      updateOneCollections(db, function () {
         client.close();
       });
       break;
     case 'update':
       updateCollections(db, function () {
+        client.close();
+      });
+      break;
+    case 'deleteone':
+      deleteOneCollections(db, function () {
+        client.close();
+      });
+      break;
+    case 'delete':
+      deleteCollections(db, function () {
+        client.close();
+      });
+      break;
+    case 'count':
+      countCollections(db, function () {
         client.close();
       });
       break;
@@ -89,22 +104,55 @@ const countCollections = function (db, callback) {
     callback(res);
   });
 }
+const updateOneCollections = function (db, callback) {
+  const collection = db.collection('documents');
+  console.time('update-multi-time');
+  collection.updateOne({ sl_no: _updateid }, { $set: { name: 'sample_name' } }, function (err, res) {
+    if (err) return console.log(err);
+    console.timeEnd('update-multi-time');
+    console.log(`Update completed`, res.result);
+    callback(res);
+  });
+}
 const updateCollections = function (db, callback) {
   const collection = db.collection('documents');
   console.time('update-time')
-  collection.updateOne({ sl_no: _updateid }, { $set: { name: 'sample_name' } }, function (err, res) {
+  collection.update({ sl_no: _updateid }, { $set: { name: 'sample_name' } }, { multi: true }, function (err, res) {
     if (err) return console.log(err);
     console.timeEnd('update-time')
     console.log(`Update completed`, res.result);
     callback(res);
   });
 }
+
 const selectOneCollections = function (db, callback) {
   const collection = db.collection('documents');
   console.time('select-time')
   collection.find({ sl_no: 9999 }).toArray(function (err, res) {
     console.timeEnd('select-time')
     console.log(`Selection completed ${res.length} records`);
+    callback(res);
+  });
+}
+
+const deleteCollections = function (db, callback) {
+  const collection = db.collection('documents');
+  console.time('delete-multi-time')
+  collection.deleteMany({ sl_no: _updateid }, function (err, res) {
+    if (err) return console.log(err);
+    console.timeEnd('delete-multi-time')
+    console.log(`delete completed`, res.deletedCount);
+    callback(res);
+  });
+}
+
+const deleteOneCollections = function (db, callback) {
+  const collection = db.collection('documents');
+  console.time('delete-time');
+  collection.deleteOne({ sl_no: _updateid }, function (err, res) {
+    if (err) return console.log(err);
+    console.timeEnd('delete-time');
+    console.log(`delete completed`, res.deletedCount);
     callback(res);
   });
 }
